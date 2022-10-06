f7ab6491db490c4d5e1108853e44cd4a48636041
Olzx <ol2az@yandex.ru>
2022-10-07 00:13:21 +0700

    added component to display selected tasks

--- src/components/SelectedTodo.vue
+++ src/components/SelectedTodo.vue
@@ -1,10 +1,27 @@
 <template>
-    
+    <ViewTodoItems
+        v-bind:todo-items="selectedTodo"
+    ></ViewTodoItems>
 </template>
 
 <script>
+import ViewTodoItems from './ViewTodoItems.vue'
+
 export default {
-    
+    props: {
+        selectedTodo: {
+            type: Array,
+            require: true
+        }
+    },
+    components: {
+        ViewTodoItems
+    },
+    data: function() {
+        return {
+            
+        }
+    }
 }
 </script>
 
