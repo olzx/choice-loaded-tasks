ed3c6d53b6fa45d7cee37adf271188146e27fdf3
Olzx <ol2az@yandex.ru>
2022-10-06 19:16:04 +0700

    added component for pagination control

--- src/components/ViewTodo.vue
+++ src/components/ViewTodo.vue
@@ -1,24 +1,29 @@
 <template>
-    <section>
-        <ViewTodoItems v-bind:todo-items="todos"></ViewTodoItems>
-        <ViewTodoPagination v-bind:amount-pages="amountPages"></ViewTodoPagination>
-    </section>
+    <ViewTodoControls
+        v-bind:todo-items="todos" 
+        v-bind:amount-pages="amountPages"
+    ></ViewTodoControls>
 </template>
 
 <script>
-import ViewTodoItems from './ViewTodoItems.vue'
-import ViewTodoPagination from './ViewTodoPagination.vue'
+import ViewTodoControls from './ViewTodoControls.vue'
 
 export default {
     data: function() {
         return {
-            todos: [],
+            todos: [
+                {
+                    id: 0,
+                    completed: true,
+                    title: 'New task',
+                    userId: 0
+                }
+            ],
             amountPages: 5
         }
     },
     components: {
-        ViewTodoItems,
-        ViewTodoPagination
+        ViewTodoControls
     },
     methods: {
         capitalizeFirstLetter: function(string) {
---
+++ src/components/ViewTodoControls.vue
@@ -0,0 +1,33 @@
+<template>
+    <section>
+        <ViewTodoItems v-bind:todo-items="todoItems"></ViewTodoItems>
+        <ViewTodoPagination v-bind:amount-pages="amountPages"></ViewTodoPagination>
+    </section>    
+</template>
+
+<script>
+import ViewTodoItems from './ViewTodoItems.vue'
+import ViewTodoPagination from './ViewTodoPagination.vue'
+
+export default {
+    props: {
+        todoItems: {
+            type: Array,
+            require: true
+        },
+        amountPages: {
+            type: Number,
+            require: true
+        }
+    },
+    components: {
+        ViewTodoItems,
+        ViewTodoPagination
+    },
+    data: function() {
+        return {
+
+        }
+    }
+}
+</script>
